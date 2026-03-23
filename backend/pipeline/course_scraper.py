c623c5fb3c2ac042baaa7c996d39fc715b777bfa
Kami <saberhqofficial@gmail.com>
2026-03-23 16:21:27 -0500

    degree scraper (Majors, Minors, Certificates) built, just need to tweak around with the Identification ID, we have 204 options now

--- backend/pipeline/course_scraper.py
+++ backend/pipeline/course_scraper.py
@@ -3,30 +3,6 @@ from bs4 import BeautifulSoup
 import time
 import json
 
-URL = "https://catalog.uic.edu/ucat/colleges-depts/engineering/cs/bs-data-science-computer-science/"
-#page = requests.get(URL)
-
-#print(page.text)
-#soup = BeautifulSoup(page.content, "html.parser")
-#courseList = soup.find_all("table", class_="sc_courselist")
-
-# testing our soup
-# print(soup.title.text)
-# print(soup.find("h1").text)
-# tables = soup.find_all("table")
-# print(len(tables)) 
-
-#for table in courseList:
- #   courses = table.find_all("a", class_="bubblelink code")
- #   for course in courses:
-   #     row = course.find_parent("tr")
-    #    tds = row.find_all("td")
-
-      #  course_name = tds[1].text.strip()
-       # numHours = tds[2].text.strip() if len(tds) > 2 else ""
-
-       # print(course["title"], course_name, numHours)
-
 subjects = [
     "ASP",   # Academic Skills Program
     "ACTG",  # Accounting
---
+++ backend/pipeline/degree_scraper.py
@@ -0,0 +1,83 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+
+URL = "https://catalog.uic.edu/ucat/degree-programs/degree-minors/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+
+degree_urls = []
+table = soup.find("table", class_="sc_degreeinfo")
+for link in table.find_all("a"):
+    href = link.get("href", "")
+    if href.startswith("/ucat/colleges-depts/"):
+        full_url = "https://catalog.uic.edu" + href
+        degree_urls.append(full_url)
+print(f"Found {len(degree_urls)} degree pages")
+
+
+degree_option_list = {}
+
+#all the degrees offered (240 for now)
+for degree in degree_urls:
+    url = degree
+    degree_page = requests.get(url)
+    degree_soup = BeautifulSoup(degree_page.content, "html.parser")
+    
+    sections = {} #sections
+    name = degree_soup.find("h1").text.strip()   #name 
+    degree_id = url.rstrip("/").split("/")[-1]  #degree ID
+
+    print(f"Scraping {degree_id}")
+
+    breadcrumbs = degree_soup.select("#breadcrumb a")
+    college = breadcrumbs[3].text.strip() if len(breadcrumbs) > 3 else ""  #the college of the degree is always 4th item in URL
+
+    #type of degree logic
+    degree_type = ""
+    if "minor" in name.lower():
+        degree_type = "minor"
+    elif "bs" in name.lower() or "ba" in name.lower():
+        degree_type = "undergraduate"
+    else:
+        degree_type = "other"
+    
+    #each table inside the pages (Gen Eds, Degree Requirements, Concentrations)
+    for h3 in degree_soup.find_all("h3"):
+        section_name = h3.text.strip()
+        section_id = section_name.lower().replace(" ", "-")
+        next_table = h3.find_next_sibling("table", class_="sc_courselist")
+        if not next_table:
+            continue  
+        courses = next_table.find_all("a", class_="bubblelink code")
+        degree_courses = []
+
+        #Building the list of courses in each sections
+        for course in courses:
+            row = course.find_parent("tr")
+            tds = row.find_all("td")
+            degree_courses.append(course["title"])
+
+        sections[section_id] = {
+            "section_name": section_name,
+            "section_hours" : None,
+            "courses": degree_courses
+        }
+        
+    degree_option_list[degree_id] = {
+        "name" : name,
+        "type_of_degree" : degree_type,
+        "college" : college,
+        "sections" :  sections
+        }
+    
+    time.sleep(1)
+
+with open("degrees.json", "w") as f:
+    json.dump(degree_option_list, f, indent=2)
+
+print(f"Done {len(degree_option_list)} courses saved.")
+
+
+        
