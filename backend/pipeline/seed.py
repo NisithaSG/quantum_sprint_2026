1ad00cba717daeca1a0e192f2160e6dc1f44d330
Kami <saberhqofficial@gmail.com>
2026-03-26 13:59:13 -0500

    fixed database strictly to pipeline

--- backend/pipeline/seed.py
+++ backend/pipeline/seed.py
@@ -1,9 +1,13 @@
 import sqlite3
 import json
+import os
 
 #defining a connection and the cursor
 
-connection = sqlite3.connect('flamemap.db')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "flamemap.db")
+
+connection = sqlite3.connect(DB_PATH)
 
 cursor = connection.cursor()
 
